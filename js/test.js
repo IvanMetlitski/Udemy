2eadc8e0e2ed62eb1d2ef2f6b49ee4a721cc3e19
Ivan Metlitski <moonydis@gmail.com>
2022-03-16 11:05:09 +0300

    Promise

---
+++ js/test.js
@@ -0,0 +1,55 @@
+'use strict'
+
+// console.log('Loading...');
+//
+// const req = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('Preparing for data...')
+//
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         }
+//
+//         resolve(product);
+//
+//     }, 2000);
+// });
+//
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//
+//         }, 2000);
+//     });
+// }).then(product => {
+//     product.modify = true;
+//     return product;
+//
+// }).then((product)=> {
+//     console.log(product);
+// }).finally(()=> {
+//     console.log('Its a final countdown');
+// });
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(()=>resolve (), time);
+    })
+}
+// test(1000).then(()=>console.log('1000 ms'));
+// test(2000).then(()=>console.log('2000 ms'));
+
+// Promise.all([test(1000)],[test(2000)]).then(()=>{
+//     console.log('All')
+// })
+Promise.race([test(1000)],[test(2000)]).then(()=>{
+    console.log('All')
+})
+
+
+
+
+
