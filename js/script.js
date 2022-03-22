163ee21c4aaade9ef9dbaaa93b79cb78e09d66c8
Ivan Metlitski <moonydis@gmail.com>
2022-03-22 16:31:27 +0300

    axios

--- js/script.js
+++ js/script.js
@@ -176,12 +176,19 @@ window.addEventListener('DOMContentLoaded', () => {
         return await result.json();
     }
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-            });
-        });
+            })
+        })
 
 //Forms
 
